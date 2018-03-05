6e4b2116bd5ccdb15b85a41227b71119c1a69f0c
yooseokseo <yos017@ucsd.edu>
2018-03-05 02:07:28 -0800

    show bookmark detail done

--- api/models/bookmark.js
+++ api/models/bookmark.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const bookmarkSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  inspirationId: { type: mongoose.Schema.Types.ObjectId, ref: 'Inspiration', required: true}
+  inspiration: { type: mongoose.Schema.Types.ObjectId, ref: 'Inspiration', required: true}
 });
 
 module.exports = mongoose.model('Bookmark', bookmarkSchema);
--- api/routes/bookmarks.js
+++ api/routes/bookmarks.js
@@ -1,45 +1,102 @@
 const express = require('express');
 const router = express.Router();
-const Bookmark = require('../models/bookmark');
 const mongoose = require("mongoose");
 
-router.get('/', (req, res, next) =>{
-    res.status(200).json({
-      message: 'bookmarks were fetched'
+const Bookmark = require('../models/bookmark');
+const Inspiration = require('../models/inspiration');
+
+
+router.get('/', (req, res, next) => {
+  Bookmark.find()
+    .select('_id inspiration')
+    .exec()
+    .then(docs => {
+      res.status(200).json({
+        count: docs.length,
+        bookmarks: docs.map(doc => {
+          return {
+            _id: doc._id,
+            inspiration: doc.inspiration,
+            request: {
+              type: 'GET',
+              url: 'http://localhost:3000/bookmarks/' + doc._id
+            }
+          }
+        })
+      });
     })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+
 });
 
-router.post('/', (req, res, next) =>{
-  const bookmark = new Bookmark({
-    _id: mongoose.Types.ObjectId(),
-    inspirationId: req.body.inspirationId
+router.post('/', (req, res, next) => {
+  Inspiration.findById(req.body.inspirationId)
+    .then(inspiration => {
+      if (!inspiration) {
+        return res.status(404).json({
+          message : "Inspiration not found"
+        });
+      }
+      const bookmark = new Bookmark({
+        _id: mongoose.Types.ObjectId(),
+        inspiration: req.body.inspirationId
 
-  });
-  bookmark.save()
-  .then( reusult => {
-    console.log(result);
-    res.status(201).json(result);
+      });
+      return bookmark.save()
+
+    })
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Bookmarked',
+        createdBookmark: {
+          _id: result._id,
+          bookmark: result.bookmark
+        },
+        request: {
+          type: 'GET',
+          url: 'http://localhost:3000/bookmarks/' + result._id
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+
+
+});
+
+router.get('/:bookmarkId', (req, res, next) => {
+  Bookmark.findById(req.params.bookmarkId)
+  .exec()
+  .then(bookmark => {
+    res.status(200).json({
+      bookmark : bookmark,
+      request: {
+        type:'GET',
+        url:'http://localhost:3000/bookmarks'
+      }
+    });
   })
   .catch(err => {
-    console.log(err);
     res.status(500).json({
-      error:err
+      error: err
     });
   });
 });
 
-router.get('/:bookmarkId', (req, res, next) =>{
-    res.status(200).json({
-      message: 'bookmarks Detail',
-      bookMarkId : req.params.bookmarkId
-    })
-});
-
-router.delete('/:bookmarkId', (req, res, next) =>{
-    res.status(200).json({
-      message: 'bookmarks were deleted',
-      bookMarkId : req.params.bookmarkId
-    })
+router.delete('/:bookmarkId', (req, res, next) => {
+  res.status(200).json({
+    message: 'bookmarks were deleted',
+    bookMarkId: req.params.bookmarkId
+  })
 });
 
 module.exports = router;
